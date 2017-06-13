fc3ef329aa763da48ade53f903b881b927070c9f
王晓萌 <wangxiaomeng@haodai.com>
2017-06-13 14:34:34 +0800

    project init

---
+++ MMBaisiDemo/android/app/src/main/java/com/mmbaisidemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mmbaisidemo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "MMBaisiDemo";
+    }
+}
